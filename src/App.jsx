568b14c3a0d1375576fd09e368444e39c499a09f
Jonathan Lew <jonlew90@gmail.com>
2026-08-25 11:18:23 -0500

    fix isOffline error

--- src/App.jsx
+++ src/App.jsx
@@ -150,9 +150,23 @@ export default function App() {
       }
     }
     document.addEventListener("mousedown", handleClickOutside);
-
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const [isOffline, setIsOffline] = useState(!navigator.onLine);
+
+  useEffect(() => {
+    const handleOnline = () => setIsOffline(false);
+    const handleOffline = () => setIsOffline(true);
+
+    window.addEventListener('online', handleOnline);
+    window.addEventListener('offline', handleOffline);
+
+    return () => {
+      window.removeEventListener('online', handleOnline);
+      window.removeEventListener('offline', handleOffline);
     };
   }, []);
   const [pendingSparksPurchase, setPendingSparksPurchase] = useState(null);
